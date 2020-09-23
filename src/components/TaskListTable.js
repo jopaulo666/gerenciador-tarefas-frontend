6db1ae025438829bdaa043436ce5fbe020fa9e3b
João Paulo da Mata Mendes <jp_cbc@hotmail.com>
2020-09-23 12:30:14 -0300

    Documentos/VisualStudioProjects/gerenciador-tarefas-frontend

--- src/components/TaskListTable.js
+++ src/components/TaskListTable.js
@@ -3,6 +3,7 @@ import TaskService from '../api/TaskService';
 import { ToastContainer, toast } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
 import { Redirect } from 'react-router-dom';
+import Alert from './Alert';
 
 class TaskListTable extends Component {
     constructor(props) {
@@ -51,6 +52,7 @@ class TaskListTable extends Component {
 
         return (
             <>
+                <Alert message="Mensagem de texte" />
                 <table className="table table-striped">
                     <TableHeader/>
                     {this.state.tasks.length > 0 ?
